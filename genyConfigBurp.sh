79ddc59fe42e2010f927e2772f7865b68a6c320d
Diego Franggi <33520681+diale13@users.noreply.github.com>
2023-08-15 19:03:03 -0300

    Update genyConfigBurp.sh
    Added placeholder

--- genyConfigBurp.sh
+++ genyConfigBurp.sh
@@ -11,7 +11,7 @@ echo "Changing directory to data/misc/user/0/cacerts-added/"
 cd data/misc/user/0/cacerts-added/
 echo "Remounting system partition as read-write"
 mount -o rw,remount /
-echo "Copying 9a5ba575.0 to /system/etc/security/cacerts"
+echo "Copying <<CA_CERT_PLACEHOLDER>> to /system/etc/security/cacerts"
 cp 9a5ba575.0 /system/etc/security/cacerts
 
 echo "Changing directory to /data/local/tmp/"
